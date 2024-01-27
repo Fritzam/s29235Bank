c3bfd5802ac7af8ead5cc4c1ee1b0c0606473204
Michał Fritza <s29235@szuflandia22.pjwstk.edu.pl>
2024-01-27 12:10:18 +0100

    Initial commit

---
+++ s29235Bank/src/main/java/com/example/s29235Bank/S29235BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s29235Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29235BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S29235BankApplication.class, args);
+	}
+
+}
